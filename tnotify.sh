02ca045a1fbe6afc024250a21847d522b2cc0e4f
Yegor Shashko <yegor.shashko@gmail.com>
2018-09-25 04:07:21 -0400

    Script upgrade
    Add param for attach sender (user) name.
    Implemented pretty result output.

--- tnotify.sh
+++ tnotify.sh
@@ -5,9 +5,9 @@
 
 BOT_TOKEN="550153967:AAH6ov8B_rFPcUJpwpEhbmmcBs-QGUHBYq0"
 USER_ID=315716801 		# use @userinfobot to know
-MESSAGE="Notify message" # init with default value
+MESSAGE="Notify message"	# init with default value
 
-# parse arguments
+# Parse arguments
 while [[ $1 != '' ]]; do
 	case $1 in
 		-m | --message )	shift
@@ -16,11 +16,24 @@ while [[ $1 != '' ]]; do
 		-u | --target-user )	shift
 					USER_ID=$1;
 					;;
+		-s | --sender-name )	SENDER_NAME=$USER
+					;;
 		* )			echo "Unknown argument: $1"
 					exit 1
 	esac
 	shift
 done
 
-echo "Sending '$MESSAGE' to user-$USER_ID";
-curl "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${USER_ID}&text=${MESSAGE}";
+# Attach sender name if required
+if [[ $SENDER_NAME ]]; then
+	MESSAGE="$SENDER_NAME: $MESSAGE"
+fi
+
+# Sendind
+echo "Sending \"$MESSAGE\" to user-$USER_ID...";
+TR_URL="https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${USER_ID}&text=${MESSAGE}";
+if ( curl -s "$TR_URL" | grep -q "\"ok\":true" ); then
+	echo "Successful!";
+else
+	echo "Something went wrong =(";
+fi
